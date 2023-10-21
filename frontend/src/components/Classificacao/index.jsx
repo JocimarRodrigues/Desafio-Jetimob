235849a9d889226644107b7e7e23068fb00651e7
John Rodrigues <jocimarrodrigues1337@gmail.com>
2023-10-21 10:33:54 -0300

    Responsividade adicionada para a Página Inicial e Navbar

--- frontend/src/components/Classificacao/index.jsx
+++ frontend/src/components/Classificacao/index.jsx
@@ -1,24 +1,18 @@
-import React from 'react'
 import PropTypes from "prop-types";
 
 import { BsFillStarFill } from "react-icons/bs";
-BsFillStarFill
+BsFillStarFill;
 
 const Classificacao = ({ classificacao }) => {
   const estrelas = [...Array(classificacao)].map((_, index) => (
-    <BsFillStarFill key={index} color='yellow'/>
+    <BsFillStarFill key={index} color="yellow" />
   ));
 
-  return (
-    <h3>
-      {estrelas}
-    </h3>
-  );
-}
+  return <h3>{estrelas}</h3>;
+};
 
 export default Classificacao;
 
 Classificacao.propTypes = {
-    classificacao: PropTypes.number,
-  };
-  
+  classificacao: PropTypes.number,
+};
